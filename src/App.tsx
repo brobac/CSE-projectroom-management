71cc6e206d8d2d335489557fda4afd0e40e8f953
brobac <issg2030@gmail.com>
2022-10-22 20:29:03 +0900

    :fire: 사용하지 않는 임시 파일,코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,8 @@
-import { Test } from "@components";
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="text-3xl underline font-bold "></div>
-      <label htmlFor="my-modal" className="btn modal-button">
-        open modal
-      </label>
-
-      <input type="checkbox" id="my-modal" className="modal-toggle" />
-      <div className="modal modal-bottom sm:modal-middle">
-        <div className="modal-box">
-          <h3 className="font-bold text-lg">
-            Congratulations random Internet user!
-          </h3>
-          <p className="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
-          </p>
-          <div className="modal-action">
-            <label htmlFor="my-modal" className="btn">
-              Yay!
-            </label>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <BrowserRouter></BrowserRouter>;
 }
 
 export default App;
