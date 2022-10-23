e200aee5b6955e7f212d2f76b46180085281f2b8
brobac <issg2030@gmail.com>
2022-10-24 02:06:13 +0900

    :bug: fix. useModal 동작안되는것 고침 모달컴포넌트가 랜더링되기전에 dom을 접근하는 경우가 발생. 완전히 해결된것인지는 불확실함

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,11 +1,18 @@
 import { MODAL_TYPE } from "@components";
+import { useEffect, useState } from "react";
 
 export const useModal = (type: typeof MODAL_TYPE[keyof typeof MODAL_TYPE]) => {
-  const target = document.getElementById(type) as HTMLInputElement;
+  const [target, setTarget] = useState<HTMLInputElement>();
 
-  const openModal = () => (target.checked = true);
-  const closeModal = () => (target.checked = false);
-  const toggleModal = () => (target.checked = !target.checked);
+  useEffect(() => {
+    setTarget(document.getElementById(type) as HTMLInputElement);
+  }, []);
+
+  const openModal = () => {
+    target!.checked = true;
+  };
+  const closeModal = () => (target!.checked = false);
+  const toggleModal = () => (target!.checked = !target!.checked);
 
   return { openModal, closeModal, toggleModal };
 };
