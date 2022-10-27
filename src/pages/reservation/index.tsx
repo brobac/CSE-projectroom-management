6b1afeb36050c9d9aa9c1f2b97f64cd6c10d2ee5
brobac <issg2030@gmail.com>
2022-10-27 22:48:17 +0900

    :sparkles: feat. 강의실 선택 영역 마크업

--- src/pages/reservation/index.tsx
+++ src/pages/reservation/index.tsx
@@ -1,10 +1,12 @@
 import { DateSelectSection } from "./_dateSelectSection";
 import { ReservationPageLayout } from "./_layout";
+import { ProjectroomSelect } from "./_projectroomSelect";
 
 export const ReservationPage = () => {
   return (
     <ReservationPageLayout>
       <DateSelectSection />
+      <ProjectroomSelect />
     </ReservationPageLayout>
   );
 };
