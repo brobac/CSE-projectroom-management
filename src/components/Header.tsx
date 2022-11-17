fe913a05daabad05babfbd25fbb27dc2521faf95
brobac <issg2030@gmail.com>
2022-11-17 17:44:23 +0900

    :sparkles: feat. 헤더 최대 너비 지정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,14 +22,16 @@ type HeaderProps = {
   rightItem?: React.ReactNode;
 };
 
-const Header = ({ title, leftItem, rightItem }: HeaderProps) => {
+export const Header = ({ title, leftItem, rightItem }: HeaderProps) => {
   return (
-    <div className="navbar fixed top-0 bg-base-100  px-4 shadow">
-      <div className="navbar-start">{leftItem}</div>
-      <div className="navbar-center">
-        <h1 className="font text-2xl font-bold text-base-content">{title}</h1>
+    <header className="fixed top-0 flex w-full justify-center bg-base-100 px-4 shadow">
+      <div className="navbar  max-w-screen-2xl">
+        <div className="navbar-start">{leftItem}</div>
+        <div className="navbar-center">
+          <h1 className="font text-2xl font-bold text-base-content">{title}</h1>
+        </div>
+        <div className="navbar-end">{rightItem}</div>
       </div>
-      <div className="navbar-end">{rightItem}</div>
-    </div>
+    </header>
   );
 };
