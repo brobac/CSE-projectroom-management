15241ff4a4c39522fd0a1e208b52b4ac0c18eb1c
brobac <issg2030@gmail.com>
2022-11-20 22:04:57 +0900

    :bug: fix. 푸터가 항상 아래쪽에 붙도록 수정

--- src/templates/ReservationServiceLayout.tsx
+++ src/templates/ReservationServiceLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 export const ReservationServiceLayout = () => {
   return (
-    <div className=" flex w-screen flex-col items-center">
+    <div className=" flex min-h-screen w-screen flex-col items-center">
       <Outlet />
       <Footer />
     </div>
