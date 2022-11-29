618596575c7c1b18c469abef5d84278787b556dd
brobac <issg2030@gmail.com>
2022-11-29 22:38:39 +0900

    :sparkles: feat. 키오스크헤더 뒤로가기버튼 추가

--- src/pages/kiosk/_header.tsx
+++ src/pages/kiosk/_header.tsx
@@ -1,7 +1,19 @@
+import { BsArrowBarLeft } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+
 export const KioskHeader = () => {
+  const navigate = useNavigate();
   return (
-    <header className=" fixed top-0 left-0 flex h-24 w-full items-center justify-center bg-base-100 bg-opacity-5 shadow-lg backdrop:blur">
-      <span className="text-6xl text-white"></span>
+    <header className=" fixed top-0 left-0 flex h-24 w-full items-center justify-between bg-base-100 bg-opacity-5 px-10 shadow-lg backdrop:blur">
+      <div></div>
+      <span className="text-6xl font-bold text-white">D330</span>
+      <button
+        onClick={() => navigate(-1)}
+        className=" btn-warning btn-lg btn flex gap-2 text-white"
+      >
+        <BsArrowBarLeft />
+        <span>뒤로가기</span>
+      </button>
     </header>
   );
 };
