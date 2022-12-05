3149ec056379066fe09b52beedb9087a75f5dd3a
brobac <issg2030@gmail.com>
2022-12-06 05:08:18 +0900

    :sparkles: feat. 키오스크 예약에서 다른프로젝트실 현황을 볼 수 있는 프로젝트실 전환 버튼 추가

--- src/pages/kiosk/kiosk-reservation/_layout.tsx
+++ src/pages/kiosk/kiosk-reservation/_layout.tsx
@@ -1,10 +1,41 @@
+import { reservationProjectRoomState, useProjectRoomListState } from "@stores";
+import { ProjectRoom } from "@types";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
+import { useRecoilState } from "recoil";
 import { KioskHeader } from "../_header";
 
 export const KioskReservationLayout = () => {
+  const { projectRoomList } = useProjectRoomListState();
+  const [unSelectedProjectRoom, setUnSelectedProjectRoom] =
+    useState<ProjectRoom>();
+  const [reservationProjectRoom, setReservationProjectRoom] = useRecoilState(
+    reservationProjectRoomState,
+  );
+
+  const onClickotherRoom = () => {
+    setReservationProjectRoom(unSelectedProjectRoom);
+  };
+
+  useEffect(() => {
+    const room = projectRoomList.find(
+      (projectroom) =>
+        projectroom.projectRoomId !== reservationProjectRoom?.projectRoomId,
+    );
+    console.log(room);
+    setUnSelectedProjectRoom(room);
+  }, [projectRoomList, reservationProjectRoom]);
+
   return (
     <>
-      <KioskHeader title="D330" />
+      <KioskHeader title={reservationProjectRoom?.roomName!}>
+        <button
+          onClick={onClickotherRoom}
+          className="btn-warning btn-lg btn text-2xl font-bold text-base-100"
+        >
+          {unSelectedProjectRoom?.roomName}
+        </button>
+      </KioskHeader>
       <div className="h-full w-full pt-24">
         <Outlet />
       </div>
