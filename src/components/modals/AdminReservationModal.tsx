a8b434d5843934cc6fbceb5ce3f1980ef048bdb8
brobac <issg2030@gmail.com>
2023-03-30 17:59:14 +0900

    :construction: 예약상세조회모달 반납사진 추가

--- src/components/modals/AdminReservationModal.tsx
+++ src/components/modals/AdminReservationModal.tsx
@@ -13,39 +13,51 @@ export const AdminReservationModal = () => {
         <div>
           <span className="text-xl font-bold">예약자 정보</span>
           <p className="flex items-center gap-1">
-            <span>로그인 ID :</span>
+            <span className="w-20 text-right">로그인 ID :</span>
             <span>{member.loginId}</span>
           </p>
           <p className="flex items-center gap-1">
-            <span>이름 :</span>
+            <span className="w-20 text-right">이름 :</span>
             <span>{member.name}</span>
           </p>
         </div>
         <div>
           <span className="text-xl font-bold">예약 정보</span>
           <p className="flex items-center gap-1">
-            <span>프로젝트실 :</span>
+            <span className="w-20 text-right">프로젝트실 :</span>
             <span>{reservation.roomName}</span>
           </p>
           <p className="flex items-center gap-1">
-            <span> 테이블 :</span>
+            <span className="w-20 text-right"> 테이블 :</span>
             <span>{reservation.tableName}</span>
           </p>
           <p className="flex items-center gap-1">
-            <span>시작 시간 :</span>
+            <span className="w-20 text-right">시작 시간 :</span>
             <span>{toYYMMDD_KO_DAY_DOT_hhmm(reservation.startAt)}</span>
           </p>
           <p className="flex items-center gap-1">
-            <span>종료시간 :</span>
+            <span className="w-20 text-right">종료시간 :</span>
             <span>{toYYMMDD_KO_DAY_DOT_hhmm(reservation.endAt)}</span>
           </p>
         </div>
         <div>
           <span className="text-xl font-bold">반납 정보</span>
-          {tableReturn.returnAt ? (
-            <div></div>
+          {tableReturn.returnedAt ? (
+            <div>
+              <p className="flex items-center gap-1">
+                <span className="w-20 text-right">반납시간 :</span>
+                <span>{toYYMMDD_KO_DAY_DOT_hhmm(tableReturn.returnedAt)}</span>
+              </p>
+              <div className="h-96  overflow-auto">
+                <img
+                  src={`${process.env.REACT_APP_IMAGE_ENDPOINT}${tableReturn.cleanUpPhoto.fileUrl}${tableReturn.cleanUpPhoto.fileLocalName}`}
+                  alt="반납사진"
+                  className=" w-full object-contain"
+                />
+              </div>
+            </div>
           ) : (
-            <div>아직 반납하지 않았습니다</div>
+            <div>반납하지 않았습니다</div>
           )}
         </div>
       </div>
