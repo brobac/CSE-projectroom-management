13db381046d72551229ec04c2648c48f805aa3a5
brobac <issg2030@gmail.com>
2023-08-26 21:03:53 +0900

    푸터에 서비스 이용안내 링크 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,6 +8,14 @@ export const Footer = () => {
   return (
     <footer className="footer mt-auto bg-base-200 py-8 px-4">
       <div className="flex gap-6">
+        <a
+          href="https://east-character-a95.notion.site/c3e18ec5387046a08d14428afe10c1aa"
+          target="_blank"
+          rel="noreferrer"
+          className="link-hover"
+        >
+          이용안내
+        </a>
         {hasAuth && (
           <button onClick={() => logout()} className="link-hover link">
             로그아웃
