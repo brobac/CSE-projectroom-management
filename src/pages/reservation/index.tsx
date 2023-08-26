82377c92508587b524b717f283675ebc7ad4f422
brobac <issg2030@gmail.com>
2023-08-26 21:05:53 +0900

    예약하기 요청중 버튼을 누르지 못하도록 비활성화

--- src/pages/reservation/index.tsx
+++ src/pages/reservation/index.tsx
@@ -5,12 +5,13 @@ import { ReservationPageLayout } from "./_layout";
 import { ProjectroomSelect } from "./_projectroomSelect";
 import { TableSelectSection } from "./_tableSelectSection";
 import { TimeSelectSection } from "./_timeSelectSection";
+import { twMerge } from "tailwind-merge";
 
 export const ReservationPage = () => {
   const { hasAuth } = useUserState();
   const { openModal } = useModal("modal-login");
 
-  const { mutate: reservation, isValid } = useReservation();
+  const { mutate: reservation, isValid, isLoading } = useReservation();
 
   const onClickReservation = () => {
     if (!hasAuth) openModal();
@@ -26,8 +27,11 @@ export const ReservationPage = () => {
       <div className="flex justify-center py-10">
         <button
           onClick={onClickReservation}
-          disabled={!isValid}
-          className="btn-primary btn-wide btn text-lg text-base-100"
+          disabled={!isValid || isLoading}
+          className={twMerge([
+            "btn-primary btn-wide btn text-lg text-base-100",
+            isLoading && "loading",
+          ])}
         >
           예약하기
         </button>
